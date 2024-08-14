9f069c7d5fc735bb8179a40e7ebce524eb176d36
DiemHang05 <tdhangdesigner@gmail.com>
2024-08-14 13:57:59 +0700

    Add files via upload

---
+++ Demsokhoangtrangtrongchuoi/Demsokhoangtrangtrongchuoi/Source.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+int countSpaces(const std::string& str) {
+	int count = 0;
+	for (char c : str) {
+		if (c == ' ') {
+			count++;
+		}
+	}
+	return count;
+}
+
+int main() {
+	std::string input;
+	std::cout << "Nhập chuỗi: ";
+	std::getline(std::cin, input);
+
+	int spaceCount = countSpaces(input);
+	std::cout << "Số lượng khoảng trắng trong chuỗi: " << spaceCount << std::endl;
+
+	return 0;
+}
