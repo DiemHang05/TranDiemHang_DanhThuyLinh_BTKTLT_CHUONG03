75ebf10228b3821f0ca3198ee12c8da5f6b13b0f
DiemHang05 <tdhangdesigner@gmail.com>
2024-08-14 13:59:15 +0700

    Add files via upload

---
+++ Inhoakytudau/Inhoakytudau/Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+std::string capitalizeWords(std::string str) {
+	bool capitalizeNext = true;
+
+	for (size_t i = 0; i < str.length(); ++i) {
+		if (std::isspace(str[i])) {
+			capitalizeNext = true;
+		}
+		else if (capitalizeNext) {
+			str[i] = std::toupper(str[i]);
+			capitalizeNext = false;
+		}
+		else {
+			str[i] = std::tolower(str[i]);
+		}
+	}
+
+	return str;
+}
+
+int main() {
+	std::string input;
+	std::cout << "Nhập chuỗi: ";
+	std::getline(std::cin, input);
+
+	std::string formattedString = capitalizeWords(input);
+	std::cout << "Chuỗi sau khi định dạng: \"" << formattedString << "\"" << std::endl;
+
+	return 0;
+}
