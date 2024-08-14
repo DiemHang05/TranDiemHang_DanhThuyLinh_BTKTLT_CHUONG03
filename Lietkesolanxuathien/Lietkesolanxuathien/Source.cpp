e43115daea9e7de8f7e29403554ab6d3d1289ff0
DiemHang05 <tdhangdesigner@gmail.com>
2024-08-14 13:59:43 +0700

    Add files via upload

---
+++ Lietkesolanxuathien/Lietkesolanxuathien/Source.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+void countCharacterOccurrences(const std::string& str) {
+	std::map<char, int> charCount;
+
+	// Duyệt qua từng ký tự trong chuỗi và tăng biến đếm cho ký tự đó
+	for (char c : str) {
+		charCount[c]++;
+	}
+
+	// In ra số lần xuất hiện của mỗi ký tự
+	for (const auto& pair : charCount) {
+		std::cout << "Ký tự '" << pair.first << "' xuất hiện " << pair.second << " lần." << std::endl;
+	}
+}
+
+int main() {
+	std::string inputString;
+
+	std::cout << "Nhập chuỗi bất kỳ: ";
+	std::getline(std::cin, inputString);
+
+	countCharacterOccurrences(inputString);
+
+	return 0;
+}
