c940e4b5296fd2054dcdb610b811a80587676455
DiemHang05 <tdhangdesigner@gmail.com>
2024-08-14 14:03:35 +0700

    Add files via upload

---
+++ Xoakhoangtrang/Xoakhoangtrang/Source.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+
+std::string removeExtraSpaces(const std::string& str) {
+	std::stringstream ss(str);
+	std::string word, result;
+	bool firstWord = true;
+
+	while (ss >> word) {
+		if (!firstWord) {
+			result += " ";
+		}
+		result += word;
+		firstWord = false;
+	}
+
+	return result;
+}
+
+int main() {
+	std::string input;
+	std::cout << "Nhập chuỗi: ";
+	std::getline(std::cin, input);
+
+	std::string cleanedString = removeExtraSpaces(input);
+	std::cout << "Chuỗi sau khi xóa khoảng trắng thừa: \"" << cleanedString << "\"" << std::endl;
+
+	return 0;
+}
